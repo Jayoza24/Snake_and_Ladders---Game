b929d920b34cc3e9e61d96c506d7e59583b5eb6a
Jayoza24 <ozajay1802@gmail.com>
2025-06-14 10:50:30 +0530

    updated ramdomness logic

--- src/utils/boardGenerator.ts
+++ src/utils/boardGenerator.ts
@@ -16,26 +16,34 @@ const getRandomInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+
 export const generateSnakesAndLadders = (): SnakeOrLadder[] => {
   const usedPositions = new Set<number>();
   const result: SnakeOrLadder[] = [];
 
+  const isConflict = (start: number, end: number) => {
+    // Check if either start or end is already used
+    return usedPositions.has(start) || usedPositions.has(end);
+  };
+
+  // Generate ladders
   while (result.filter((e) => e.type === "ladder").length < ENTITY_COUNT) {
     const bottom = getRandomInt(MIN_CELL, MAX_CELL - 10);
     const top = getRandomInt(bottom + 1, 100);
 
-    if (!usedPositions.has(bottom) && !usedPositions.has(top)) {
+    if (!isConflict(bottom, top)) {
       result.push({ start: bottom, end: top, type: "ladder" });
       usedPositions.add(bottom);
       usedPositions.add(top);
     }
   }
 
+  // Generate snakes
   while (result.filter((e) => e.type === "snake").length < ENTITY_COUNT) {
     const head = getRandomInt(MIN_CELL + 10, MAX_CELL);
     const tail = getRandomInt(1, head - 1);
 
-    if (!usedPositions.has(head) && !usedPositions.has(tail)) {
+    if (!isConflict(head, tail)) {
       result.push({ start: head, end: tail, type: "snake" });
       usedPositions.add(head);
       usedPositions.add(tail);
